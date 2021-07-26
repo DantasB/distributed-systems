060a736d6ec57cea5bea7864916c79e64ab4af4d
Bruno Dantas <bruno.d.paiva@hotmail.com>
2021-07-26 00:14:07 -0300

    Adding signal blocking waiting

---
+++ Trabalho_1/Signals/receiver.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+func busy_wait(signal_chan chan os.Signal, exit_chan chan int) {
+
+}
+
+func blocking_waiting(signal_chan chan os.Signal, exit_chan chan int) {
+	go func() {
+		for {
+			signal := <-signal_chan
+			switch signal {
+			case syscall.SIGHUP:
+				fmt.Println("[RECEIVER] SIGHUP received.")
+
+			case syscall.SIGTERM:
+				fmt.Println("[RECEIVER] SIGTERM received.")
+
+			case syscall.SIGQUIT:
+				fmt.Println("[RECEIVER] SIGQUIT received.")
+				exit_chan <- 0
+			}
+		}
+	}()
+}
+
+func main() {
+	fmt.Printf("pid: %d\n", os.Getpid())
+	signal_chan := make(chan os.Signal, 1)
+
+	signal.Notify(signal_chan)
+
+	exit_chan := make(chan int)
+
+	busy_wait(signal_chan, exit_chan)
+
+	code := <-exit_chan
+
+	fmt.Println("[RECEIVER] Process finished.")
+
+	os.Exit(code)
+}
