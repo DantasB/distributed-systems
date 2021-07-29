12335b698940d6fc58e948779d955828f8c698d5
Bruno Dantas <bruno.d.paiva@hotmail.com>
2021-07-28 23:34:15 -0300

    Adding signals comments

--- Trabalho_1/Signals/receiver.go
+++ Trabalho_1/Signals/receiver.go
@@ -8,11 +8,19 @@ import (
 	"syscall"
 )
 
-func busy_wait(signal_chan chan os.Signal, exit_chan chan int) {
+// BusyWait receives the signal channel and the exit channel and prints messages when receive some signals.
+// It uses a goroutine, a go way to use concurrent programming.
+// To sync the concurrency it's necessary to use a channel (in this case a Signal Channel).
+// The while true loop have a select-case that checks if the signal was received and waits in this condition.
+// But because it has a default sentence it will enter in this section every time the loop runs.
+// The signals treated are SIGHUP (1), SIGTERM(15), SIGQUIT(3).
+// The exitChan receives a 0 message exiting of the goroutine.
+// It has no return.
+func busyWait(signalChan chan os.Signal, exitChan chan int) {
 	go func() {
 		for {
 			select {
-			case signal := <-signal_chan:
+			case signal := <-signalChan:
 
 				if signal == syscall.SIGHUP {
 					fmt.Println("[RECEIVER] SIGHUP received.")
@@ -22,7 +30,7 @@ func busy_wait(signal_chan chan os.Signal, exit_chan chan int) {
 				}
 				if signal == syscall.SIGQUIT {
 					fmt.Println("[RECEIVER] SIGQUIT received.")
-					exit_chan <- 0
+					exitChan <- 0
 				}
 			default:
 			}
@@ -30,10 +38,18 @@ func busy_wait(signal_chan chan os.Signal, exit_chan chan int) {
 	}()
 }
 
-func blocking_wait(signal_chan chan os.Signal, exit_chan chan int) {
+// BlockingWait receives the signal channel and the exit channel and prints messages when receive some signals.
+// It uses a goroutine, a go way to use concurrent programming.
+// To sync the concurrency it's necessary to use a channel (in this case a Signal Channel).
+// The variable signal blocks the process until the signalChann variable receives a signal.
+// The while true loop have a switch-case that checks every signal received.
+// The signals treated are SIGHUP (1), SIGTERM(15), SIGQUIT(3).
+// The exitChan receives a 0 message exiting of the goroutine.
+// It has no return.
+func blockingWait(signalChan chan os.Signal, exitChan chan int) {
 	go func() {
 		for {
-			signal := <-signal_chan
+			signal := <-signalChan
 			switch signal {
 			case syscall.SIGHUP:
 				fmt.Println("[RECEIVER] SIGHUP received.")
@@ -43,10 +59,25 @@ func blocking_wait(signal_chan chan os.Signal, exit_chan chan int) {
 
 			case syscall.SIGQUIT:
 				fmt.Println("[RECEIVER] SIGQUIT received.")
-				exit_chan <- 0
+				exitChan <- 0
 			}
 		}
 	}()
+}
+
+// InstantiateChannels receives nothing as parameters and returns a tuple containing 2 channels.
+// It instantiate the signalChannel that is a channel that receives the os.Signal values.
+// It instantiate the exitChannel that is a channel that receives the int values.
+// It receives the notification of every signal sent, avoiding any signal utility unless the defined by the previous functions.
+// It has no return.
+func instantiateChannels() (chan os.Signal, chan int) {
+	signalChannel := make(chan os.Signal, 1)
+
+	signal.Notify(signalChannel)
+
+	exitChannel := make(chan int)
+
+	return signalChannel, exitChannel
 }
 
 func main() {
@@ -57,24 +88,20 @@ func main() {
 
 	fmt.Printf("[RECEIVER] Process pid : %d\n", os.Getpid())
 
-	signal_chan := make(chan os.Signal, 1)
-
-	signal.Notify(signal_chan)
-
-	exit_chan := make(chan int)
+	signalChan, exitChan := instantiateChannels()
 
 	mode, _ := strconv.Atoi(os.Args[1])
 	switch mode {
 	case 0:
-		blocking_wait(signal_chan, exit_chan)
+		blockingWait(signalChan, exitChan)
 	case 1:
-		busy_wait(signal_chan, exit_chan)
+		busyWait(signalChan, exitChan)
 	default:
 		fmt.Println("[RECEIVER] Unknown parameter. Please, choose 0 for blocking wait or 1 for busy wait.")
 		return
 	}
 
-	code := <-exit_chan
+	code := <-exitChan
 
 	fmt.Println("[RECEIVER] Process finished.")
 
