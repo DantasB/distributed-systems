824b084d3e6172861e5cc455872e361b8029e76d
Bruno Dantas <bruno.d.paiva@hotmail.com>
2021-07-28 23:34:21 -0300

    Adding sockets comments

--- Trabalho_1/Sockets/server.go
+++ Trabalho_1/Sockets/server.go
@@ -8,10 +8,19 @@ import (
 	"os"
 )
 
+// GetSquareRoot receives an integer number and returns this square root.
+// It's necessary to cast the integer to float64 because of the sqrt function.
+// Ceil the obtained square root because the output is float64.
+// Convert again to integer because of the output of this function.
+// It returns the square root of a number.
 func getSquareRoot(number int) int {
 	return int(math.Ceil(math.Sqrt(float64(number))))
 }
 
+// IsPrime receives an integer number and returns a string.
+// It will iterate over 2 to the square root of the number - 1.
+// Check if the number is divisible by the i.
+// It returns the string false or true.
 func isPrime(number int) string {
 	for i := 2; i < getSquareRoot(number); i++ {
 		if number%i == 0 {
