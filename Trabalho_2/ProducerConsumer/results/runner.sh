dd02666c78f9fd78c6b7312f3ec6886f1dcd26b1
Bruno Dantas <bruno.d.paiva@hotmail.com>
2021-08-15 00:49:11 -0300

    Minor changes on bash script

--- Trabalho_2/ProducerConsumer/results/runner.sh
+++ Trabalho_2/ProducerConsumer/results/runner.sh
@@ -22,7 +22,7 @@ do
     for element in "${np_ns[@]}"; 
     do
         read -a np_nc <<< "$element"
-        echo "Running for NP = ${np_nc[0]}; NC = ${np_nc[1]}; N= ${N[$n]}"
+        echo "Running for NP = ${np_nc[0]}; NC = ${np_nc[1]}; N = ${N[$n]}"
         for (( i=0; i<10;i++));
         do
 	        go run ../main.go -n=${N[$n]} -np=${np_nc[0]} -nc=${np_nc[1]} > tmp.txt
