f470554b0ba441f4fdfeb29070f8f3385e59387b
Bruno Dantas <bruno.d.paiva@hotmail.com>
2021-10-10 14:05:11 -0300

    Adding comments to constants file

--- Trabalho_3/Utils/constants.go
+++ Trabalho_3/Utils/constants.go
@@ -1,5 +1,6 @@
 package utils
 
+//Those are the mask of all the messages defined to this code
 const Message_mask uint32 = 0xC0000000
 const Process_mask uint32 = 0x3FFFFFFF
 const Error_message uint32 = 0x00000000
