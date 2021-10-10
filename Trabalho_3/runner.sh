efc621ccff59c2722ee0455ff1a0432a19c5f361
Bruno Dantas <bruno.d.paiva@hotmail.com>
2021-10-10 00:50:27 -0300

    Update runner.sh to check the logs validation

--- Trabalho_3/runner.sh
+++ Trabalho_3/runner.sh
@@ -1,8 +1,10 @@
 
 COORDINATOR_PATH=Coordinator/
-PROCESS_PATH=Process/
+PROCESS_PATH=Process
 
-cd $PROCESS_PATH
+cd $PROCESS_PATH/
+ > resultado.txt
+
 NUMBER_OF_PROCESSES=$1
 R=$2
 K=$3
@@ -10,3 +12,8 @@ for PROCESS_NUMBER in $(seq 1 $NUMBER_OF_PROCESSES);
 do
     go run process.go -pn=$PROCESS_NUMBER -r=$R -k=$K &
 done
+
+cd ../Validation/
+
+sleep $(($K*$R*2))
+go run validator.go -r=$R -n=$NUMBER_OF_PROCESSES
